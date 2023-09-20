2b719612127dc29233888d1d0aa5244d7047dea6
puly garcia <pulygarcia09@gmail.com>
2023-09-20 11:06:34 -0300

    adding datepicker calendar

--- src/views/admin/SalesView.vue
+++ src/views/admin/SalesView.vue
@@ -1,9 +1,17 @@
 <script setup>
-
+  import VueTailwindDatepicker from 'vue-tailwind-datepicker';
 </script>
 
 <template>
-  <div>
     <h1 class="my-10 text-4xl font-black">Resumen de ventas</h1>
-  </div>
+
+    <div class="md:flex md:items-start mt-10 gap-10">
+      <div class="md:w-1/3 bg-white">
+        <VueTailwindDatepicker />
+      </div>
+
+      <div class="md:w-2/3">
+        <p>ventas</p>
+      </div>
+    </div>
 </template>
